13a80ad078368e96e73fbbb65f17bfb1eddd028b
Robinpreet3374@gmail.com <Robinpreet3374@gmail.com>
2021-10-28 17:37:48 +0300

    cc

---
+++ EmployeeInformationApp_JatinGroup/EmployeeInformationApp_JatinGroup/ISQLite.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeInformationApp_JatinGroup.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeInformationApp_JatinGroup
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+        bool SaveEmployee(Employee employee);
+        List<Employee> GetEmployees();
+        bool UpdateEmployee(Employee employee);
+        void DeleteEmployee(int id);
+
+    }
+}
